5041aeaf94bd25a3a4d5b43bcb5755af78e93e6d
Gronax <mertavci13@gmail.com>
2019-08-04 15:21:13 +0300

    babel and react added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/dist',
+    publicPath: '/',
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
   }
 };
