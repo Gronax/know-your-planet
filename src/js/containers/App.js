7434f2de2365970f77a89b5e10d01d6a9f4a5ee1
Gronax <mertavci13@gmail.com>
2019-11-30 19:43:22 +0300

    autocomplete added

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import CountrySelect from '../components/Autocomplete'
 
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
+        <CountrySelect />
       </div>
     )
   }
